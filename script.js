73c649d236a47945ddba848effa53f7a3f558763
teng2charity <emailcharityhere@gmail.com>
2025-11-06 17:24:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+ 
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.getElementById("gallery");
+  const box = document.querySelector(".box");
+  const colorButton = document.getElementById("changeColor");
+  const toggleButton = document.getElementById("toggleImage");
+  
+  colorButton.addEventListener("click", () => {
+    box.style.backgroundColor = 
+    box.style.backgroundColor === "blue" ? "red" : "blue"; 
+  }); 
+ 
+  let toggled = false;
+  toggleButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/image1.jpg";
+      toggled = false;
+    } else {
+      gallery.src = "./assets/image2.jpg"; 
+      toggled = true;
+    }
+  });
+ 
+  console.log("Page loaded!")
+});
+ 
